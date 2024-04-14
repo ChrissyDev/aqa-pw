87ef14ea0c8ed5cf4459737bfdb0d7450effcf00
ChrissyDev <stg.team01@gmail.com>
2024-04-14 16:22:02 +0300

    homework-1 fix-1

--- tests/qauto/homework1.spec.js
+++ tests/qauto/homework1.spec.js
@@ -1,70 +1,74 @@
 import { test, expect } from "@playwright/test";
+import { faker } from '@faker-js/faker';
+
+
+const signupName = faker.person.firstName();
+const signupLastName = faker.person.lastName();
+const signupEmail = faker.internet.email();
+const signupPassword = faker.internet.password();
 
 
 test.describe('Registration form test', () => {
 
     test.beforeEach(async ({page})=>{
-        await page.goto("https://qauto.forstudy.space/");
+        await page.goto("/");
         await page.locator(".btn-primary").click();
         await expect(page.locator(".modal-content")).toBeVisible();
     })
 
   test('Positive', async ({ page }) => {
-    await page.locator("#signupName").fill("Justin");
-    await page.locator("#signupLastName").fill("Potter");
-    await page.locator("#signupEmail").fill("dns-jp@test.com");
-    await page.locator("#signupPassword").fill("qwertY@123");
-    await page.locator("#signupRepeatPassword").fill("qwertY@123");
+    await page.locator("#signupName").fill(signupName);
+    await page.locator("#signupLastName").fill(signupLastName);
+    await page.locator("#signupEmail").fill(signupEmail);
+    await page.locator("#signupPassword").fill(signupPassword);
+    await page.locator("#signupRepeatPassword").fill(signupPassword);
+
 
     const name = await page.locator("#signupName").evaluate(element => element.value);
-    const expectedName = "Justin";
-    expect(name).toBe(expectedName);
+    expect(name).toBe(signupName);
 
     const lastName = await page.locator("#signupLastName").evaluate(element => element.value);
-    const expectedLastName = "Potter";
-    expect(lastName).toBe(expectedLastName);
+    expect(lastName).toBe(signupLastName);
 
     const email = await page.locator("#signupEmail").evaluate(element => element.value);
-    const expectedEmail = "dns-jp@test.com";
-    expect(email).toBe(expectedEmail);
+    expect(email).toBe(signupEmail);
 
     const passwordValue = await page.locator("#signupPassword").evaluate(element => element.value);
-    const expectedValue = "qwertY@123";
-    expect(passwordValue).toBe(expectedValue);
+    expect(passwordValue).toBe(signupPassword);
 
     const passworRepeatdValue = await page.locator("#signupPassword").evaluate(element => element.value);
-    const expectedRepeatValue = "qwertY@123";
-    expect(passworRepeatdValue).toBe(expectedRepeatValue);
+    expect(passworRepeatdValue).toBe(signupPassword);
 
     await page.getByText("Register").click();
-    await expect(page).toHaveURL('https://qauto.forstudy.space/panel/garage')
+    await expect(page).toHaveURL('/' + 'panel/garage')
   });
 
   test('Empty fields', async ({page})=>{
     await page.locator("#signupName").click();
     await page.locator("#signupLastName").click();
-    await expect(page.getByText("Name required")).toBeVisible();
+    await expect(page.locator("#signupName + .invalid-feedback")).toBeVisible();
     await page.locator("#signupEmail").click();
-    await expect(page.getByText("Last name required")).toBeVisible();
+    await expect(page.locator("#signupLastName + .invalid-feedback")).toBeVisible();
     await page.locator("#signupPassword").click();
-    await expect(page.getByText("Email required")).toBeVisible();
+    await expect(page.locator("#signupEmail + .invalid-feedback")).toBeVisible();
     await page.locator("#signupRepeatPassword").click();
-    await expect(page.getByText("Password required")).toBeVisible();
+    await expect(page.locator("#signupPassword + .invalid-feedback")).toBeVisible();
     await page.getByText("Registration").click();
-    await expect(page.getByText("Re-enter password required")).toBeVisible();
+    await expect(page.locator("#signupRepeatPassword + .invalid-feedback")).toBeVisible();
   })
-
-  //Цей не стабільно працює, треба якесь одне значення для ргб, можливо навіть переробити на expect().not.ToBe
-  test.only('Check red border', async ({page})=>{
+  
+  test('Check red border', async ({ page }) => {
     await page.locator("#signupName").click();
-    const elementHandle = await page.locator('#signupName');
-    const computedStyle = await elementHandle.evaluate(element => {
-        const style = window.getComputedStyle(element);
-        return style.borderColor;
-      });
-
-      expect(computedStyle).toBe('rgb(220, 53, 69)');
-  })
-
+    await page.locator("#signupLastName").click();
+    await expect(page.locator("#signupName")).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+    await page.locator("#signupEmail").click();
+    await expect(page.locator("#signupLastName")).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+    await page.locator("#signupPassword").click();
+    await expect(page.locator("#signupEmail")).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+    await page.locator("#signupRepeatPassword").click();
+    await expect(page.locator("#signupPassword")).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+    await page.getByText("Registration").click();
+    await expect(page.locator("#signupRepeatPassword")).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+});
 
 });
